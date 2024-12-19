588e876236558d660fa1a37c0cd9b9bd85a97d68
Piotr Slawecki <sp9dev@gmail.com>
2024-12-19 21:29:54 +0100

    checker works

--- productions/p11/production11.py
+++ productions/p11/production11.py
@@ -15,36 +15,33 @@ class ProductionP11(Production):
         Sprawdza, czy produkcja może zostać zastosowana do wybranego sześciokąta,
         który posiada dokładnie 2 wiszące węzły h=1 na sąsiadujących krawędziach.
         """
+
+        # Zliczenie hanging nodes
+        h_cnt = 0
         for node, data in self.graph.nodes(data=True):
+            if data.get("h") == 1:
+                h_cnt += 1
+        
+        print(h_cnt)
 
-            if data.get("label") == "P" and data.get("R") == 1:
-                neighbors_nodes = list(self.graph.neighbors(node))
-                print(neighbors_nodes)
-                print([(n, self.graph.nodes[n].get("h")) for n in neighbors_nodes])
-                hanging_nodes = [n for n in neighbors_nodes if self.graph.nodes[n].get("h") == 1]
-                print(len(hanging_nodes))
-                if len(hanging_nodes) == 2:
-                    print(self.graph.has_edge(hanging_nodes[0], hanging_nodes[1]))
-                    if self.graph.has_edge(hanging_nodes[0], hanging_nodes[1]):
-
-                        non_hanging_nodes = [n for n in neighbors_nodes if self.graph.nodes[n].get("h") == 0]
-
-                        if len(non_hanging_nodes) == 5:
-
-                            neighbors_edges_cnt = 0
-                            for n1, n2 in combinations(neighbors_nodes, 2):
-                                if self.graph.has_edge(n1, n2):
-                                    neighbors_edges_cnt += 1
-                            print(neighbors_edges_cnt)
-                            if neighbors_edges_cnt == 7:
-                                print("prod 11 can be applied to this graph with 2 adjacent hanging nodes")
-                                return self._extract_subgraph(node, neighbors_nodes)
-
+        # Sprawdzenie, czy hanging nodes sąsdiają ze sobą
+        if h_cnt != 2:
+            print("prod 11 can't be applied to this graph")
+            return False
+        hanging_nodes_neighbors = set()
+        for node, data in self.graph.nodes(data=True):
+            if data.get("h") == 1:
+                for neighbor in self.graph.neighbors(node):
+                    hanging_nodes_neighbors.add(neighbor)
+        if len(hanging_nodes_neighbors) == 3:
+            
+            return True
         print("prod 11 can't be applied to this graph")
-        return None
+        return False
+    
 
     def apply(self):
-        """Zastosowanie produkcji P11 dla przypadku dwóch wiszących sąsiadujących węzłów."""
+        """Apply P10 to divide the hexagon."""
         result = self.check
 
         if not result:
@@ -54,17 +51,35 @@ class ProductionP11(Production):
         self.subgraph.remove_node(q_node)
         self.graph.remove_node(q_node)
 
+        h_nodes = {}
+        for n, n_attrs in self.graph.nodes.items():
+            if n_attrs.get("h") == 1:
+                v_neighbors = list(self.graph.neighbors(n))
+                if len(v_neighbors) != 2:
+                    raise ValueError("Hanging node has more than 2 neighbors")
+                n1, n2 = v_neighbors
+                b1 = self.graph.get_edge_data(n, n1).get("B")
+                b2 = self.graph.get_edge_data(n, n2).get("B")
+                if b1 != b2:
+                    raise ValueError("Hanging node has different B values")
+                h_nodes[n] = {"neighbors": v_neighbors, "B": b1}
+
+        for n in h_nodes.keys():
+            self.graph.remove_node(n)
+
         midpoints = {}
         for (n1, n2) in combinations(nodes, 2):
-
             if self.subgraph.get_edge_data(n1, n2):
                 self._create_midpoint(midpoints, n1, n2)
 
-            elif self.subgraph.degree(n1) == 1 and self.subgraph.degree(n2) == 1:
-                self.subgraph.add_edge(n1, n2, label='E', B=1)
-                self.graph.add_edge(n1, n2)
-                _ = self._create_midpoint(midpoints, n1, n2)
+        for n_attrs in h_nodes.values():
+            n1, n2 = n_attrs["neighbors"]
+            B = n_attrs["B"]
+            self.subgraph.add_edge(n1, n2, label='E', B=B)
+            self.graph.add_edge(n1, n2)
+            _ = self._create_midpoint(midpoints, n1, n2)
 
         self._fill_graph(nodes, midpoints)
 
+        # Replace subgraph in graph
         self.graph.update(self.subgraph)
