1ff5f196a381870913c38efb96a3e63fc722b450
Piotr Slawecki <sp9dev@gmail.com>
2024-12-19 21:49:27 +0100

    P12 wymaga zmian ale dziala

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fake_graphs import *
 from plot_graph import plot_graph
-from productions.p11.production11 import ProductionP11
+from productions.p12.production12 import ProductionP12
 
 if __name__ == '__main__':
     G = nx.Graph()
     # Add the center node
+# Add the center node
     G.add_node("S:5.0:5.0", label="P", R=1)
 
     # Add nodes to form a hexagon with two hanging nodes
@@ -18,23 +19,21 @@ if __name__ == '__main__':
 
         # Hanging nodes (on edges)
         ("v:5.0:8.66", {"label": "v", "x": 5.0, "y": 8.66, "h": 1}),  # Hanging node on top edge
-        ("v:8.75:6.83", {"label": "v", "x": 8.75, "y": 6.83, "h": 1}),  # Hanging node on bottom edge
+        ("v:5.0:1.34", {"label": "v", "x": 5.0, "y": 1.34, "h": 1}),  # Hanging node on bottom edge
     ])
 
     # Add edges to form the hexagon and connections to hanging nodes
     G.add_edges_from([
         ("v:0.0:5.0", "v:2.5:8.66", {"label": "E", "B": 1}),
+        ("v:7.5:8.66", "v:10.0:5.0", {"label": "E", "B": 1}),
         ("v:10.0:5.0", "v:7.5:1.34", {"label": "E", "B": 1}),
-        ("v:7.5:1.34", "v:2.5:1.34", {"label": "E", "B": 1}),
         ("v:2.5:1.34", "v:0.0:5.0", {"label": "E", "B": 1}),
 
         # Hanging node connections
         ("v:2.5:8.66", "v:5.0:8.66", {"label": "E", "B": 1}),
         ("v:7.5:8.66", "v:5.0:8.66", {"label": "E", "B": 1}),
-        ("v:7.5:8.66", "v:8.75:6.83", {"label": "E", "B": 1}),
-        ("v:8.75:6.83", "v:10.0:5.0", {"label": "E", "B": 1}),
-
-
+        ("v:7.5:1.34", "v:5.0:1.34", {"label": "E", "B": 1}),
+        ("v:2.5:1.34", "v:5.0:1.34", {"label": "E", "B": 1}),
 
         # Connections to center node
         ("S:5.0:5.0", "v:0.0:5.0"),
@@ -44,8 +43,7 @@ if __name__ == '__main__':
         ("S:5.0:5.0", "v:7.5:1.34"),
         ("S:5.0:5.0", "v:2.5:1.34"),
     ])
-
     plot_graph(G)
-    prod10 = ProductionP11(G)
+    prod10 = ProductionP12(G)
     prod10.apply()
     plot_graph(G)
--- productions/p12/production12.py
+++ productions/p12/production12.py
@@ -1,57 +1,107 @@
-from lib import Production, attr, next_nodes, LABEL
-from networkx import Graph
+from itertools import combinations
 
-#    E-----E          E-----E
-#    |\   /|          |\   /|
-#    |  I  |   =>     |  i  |
-#    |/   \|          |/ | \|
-#    E-----E          E--|--E
-#                        |
-#                        |
-#                     E--|--E
-#                     |\ | /|
-#                     |  I  |
-#                     |/   \|
-#                     E-----E
+from productions.production import Production
 
 
-EL1, EL2, EL3, EL4, IL = next_nodes(5)
+class ProductionP12(Production):
+    def extract_left_side(self) -> None | tuple:
+        for node, data in self.graph.nodes(data=True):
+            if not is_splittable_hyperedge_node(data):
+                continue
+
+            neighbors = list(self.graph.neighbors(node))
+            if not self._is_valid_neighbors(neighbors):
+                continue
+
+            two_hanging_middles = self._find_two_hanging_middles(neighbors)
+            if not two_hanging_middles:
+                continue
+
+            n1, m12, n2, n3, m34, n4 = two_hanging_middles
+            if self._is_valid_middle(n1, m12, n2) and self._is_valid_middle(
+                n3, m34, n4
+            ):
+                return (
+                    self._extract_subgraph(node, neighbors + [m12, m34]),
+                    two_hanging_middles,
+                )
+        return None
+
+    def apply(self):
+        result = self.extract_left_side()
+        if result is not None:
+            (q, neighbors), (n1, m12, n2, n3, m34, n4) = result
+            self._remove_nodes(q, neighbors, m12, m34)
+
+            midpoints = {m12: (n1, n2), m34: (n3, n4)}
+            self._create_midpoints(neighbors, midpoints)
+
+            phantom_edge_data = {
+                (n1, n2): self.graph.get_edge_data(n1, m12),
+                (n3, n4): self.graph.get_edge_data(n3, m34),
+            }
+            self._fill_graph(neighbors, midpoints, edge_data=phantom_edge_data)
+            self._update_subgraph_nodes(m12, m34)
+
+            self.graph.update(self.subgraph)
+
+    def _is_valid_neighbors(self, neighbors) -> bool:
+        return self._all_are_not_hanging_node(neighbors) and len(neighbors) == 6
+
+    def _remove_nodes(self, q, neighbors, m12, m34):
+        neighbors.remove(m12)
+        neighbors.remove(m34)
+        self.subgraph.remove_node(q)
+        self.graph.remove_node(q)
+
+    def _create_midpoints(self, neighbors, midpoints):
+        for a, b in combinations(neighbors, 2):
+            if self.subgraph.has_edge(a, b):
+                self._create_midpoint(midpoints, a, b)
+
+    def _update_subgraph_nodes(self, m12, m34):
+        self.subgraph.nodes[m12]["h"] = 0
+        self.subgraph.nodes[m34]["h"] = 0
+
+    def _all_are_not_hanging_node(self, neighbors) -> bool:
+        return all(self.graph.nodes[n].get("h") == 0 for n in neighbors)
+
+    def _find_two_hanging_middles(self, neighbors) -> None | list:
+        hanging_middles = []
+        for a, b in combinations(neighbors, 2):
+            m = self._find_middlepoint(a, b)
+            if self._is_hanging_middle(m):
+                hanging_middles += [a, m, b]
+        if len(set(hanging_middles)) == 6:
+            return hanging_middles
+        return None
+
+    def _find_middlepoint(self, n1, n2) -> None | str:
+        for n in self.graph.neighbors(n1):
+            if (
+                n in self.graph.neighbors(n2)
+                and not is_hyperedge_node(self.graph.nodes[n])
+                and self.graph.nodes[n].get("h") == 1
+            ):
+                return n
+        return None
+
+    def _is_valid_middle(self, a, middle, b) -> bool:
+        if not self._is_hanging_middle(middle):
+            return False
+
+        data1 = self.graph.get_edge_data(a, middle)
+        data2 = self.graph.get_edge_data(b, middle)
+
+        return data1["B"] == data2["B"]
+
+    def _is_hanging_middle(self, middle) -> bool:
+        return middle is not None and self.graph.nodes[middle].get("h") == 1
 
 
-def __get_node_pos(graph, node_id):
-  return graph.nodes[node_id]["x"], graph.nodes[node_id]["y"]
+def is_splittable_hyperedge_node(data) -> bool:
+    return is_hyperedge_node(data) and data.get("R") == 1
 
 
-def production_left_side():
-    left = Graph()
-    left.add_nodes_from([(EL1, attr("E")), (EL2, attr("E")), (EL3, attr("E")), (EL4, attr("E")), (IL, attr("I"))])
-    left.add_edges_from([(EL1, EL2), (EL2, EL3), (EL3, EL4), (EL4, EL1), (EL1, IL), (EL2, IL), (EL3, IL), (EL4, IL)])
-    return left
-
-
-def production_modification(graph: Graph, mapping: dict) -> Graph:
-    E1, E2, E3, E4 = next_nodes(4)
-    I1 = next_nodes(1)
-    level = graph.nodes[mapping[EL1]]["level"] + 1
-    graph.nodes[mapping[IL]][LABEL] = "i"
-
-    x1, y1 = __get_node_pos(graph, mapping[EL1])
-    x2, y2 = __get_node_pos(graph, mapping[EL2])
-    x3, y3 = __get_node_pos(graph, mapping[EL3])
-    x4, y4 = __get_node_pos(graph, mapping[EL4])
-    xi, yi = __get_node_pos(graph, mapping[IL])
-
-    graph.add_nodes_from([
-        (E1, attr("E", x1, y1, level)),
-        (E2, attr("E", x2, y2, level)),
-        (E3, attr("E", x3, y3, level)),
-        (E4, attr("E", x4, y4, level)),
-    ])
-    graph.add_nodes_from([(I1, attr("I", xi, yi, level))])
-
-    graph.add_edges_from([(E1, E2), (E2, E3), (E3, E4), (E4, E1)])
-    graph.add_edges_from([(E1, I1), (E2, I1), (E3, I1), (E4, I1)])
-    graph.add_edges_from([(mapping[IL], I1)])
-
-
-P12 = Production(production_left_side(), production_modification)
+def is_hyperedge_node(data) -> bool:
+    return data.get("label") == "P"
